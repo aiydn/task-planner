f15020fdb0d18ccae44e448d96e30f28e131f17f
aiydn <48388034+aiydn@users.noreply.github.com>
2024-05-25 00:03:04 +0200

    Update ReloadPrompt.vue

--- src/components/ReloadPrompt.vue
+++ src/components/ReloadPrompt.vue
@@ -14,7 +14,7 @@ async function close() {
 </script>
 
 <template>
-  <div v-if="offlineReady || needRefresh" class="toast toast-top toast-center ">
+  <div v-if="offlineReady || needRefresh" class="z-50 toast toast-top toast-center ">
     <div v-if="offlineReady" role="alert" class="alert ">
       <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
