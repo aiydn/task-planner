5ca3c01ee1c55f89f96ce24a002996118ee52de5
aiydn <48388034+aiydn@users.noreply.github.com>
2024-05-31 18:23:34 +0200

    week number fix

--- src/components/Planner.vue
+++ src/components/Planner.vue
@@ -27,10 +27,16 @@ function randomGenCheck() {
 }
 
 function getNumberOfWeek() {
-  const today = new Date();
-  const firstDayOfYear = new Date(today.getFullYear(), 0, 1);
-  const pastDaysOfYear = (today.valueOf() - firstDayOfYear.valueOf()) / 86400000;
-  return "Week " + Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+  var date = new Date();
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  const number =  1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
+  return "Week " + number;
+
 }
 
 const dropdown = ref({
